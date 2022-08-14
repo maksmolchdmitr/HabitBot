bdc7853579645cd6d0f49f0654ccd23ceaa9e4e5
maksmolchdmitr <62752481+maksmolchdmitr@users.noreply.github.com>
2022-08-14 12:23:42 +0300

    first commit

---
+++ src/main/java/com/MamaKapa/HabitBot/HabitBotApplication.java
@@ -0,0 +1,13 @@
+package com.MamaKapa.HabitBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MamaKapa/HabitBot/HabitBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MamaKapa.HabitBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
